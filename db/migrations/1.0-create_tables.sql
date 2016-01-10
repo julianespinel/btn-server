c51345c0638c3057ab5f750b0828b8c10f3c792c
Julian Espinel <julianespinel@gmail.com>
2016-01-10 07:49:18 -0500

    Add elders table, add foreign keys as needed.

--- db/migrations/1.0-create_tables.sql
+++ db/migrations/1.0-create_tables.sql
@@ -1,4 +1,5 @@
 -- +migrate Up
 CREATE TABLE panic_devices (serial VARCHAR(128) PRIMARY KEY, birth_date TIMESTAMP NOT NULL, death_date TIMESTAMP);
-CREATE TABLE devices_elders (serial VARCHAR(128), elder_id VARCHAR(128), PRIMARY KEY (serial, elder_id));
-CREATE TABLE devices_history (serial VARCHAR(128) NOT NULL, elder_id VARCHAR(128) NOT NULL, attachmentDate TIMESTAMP NOT NULL, detachmentDate TIMESTAMP);
+CREATE TABLE elders (id VARCHAR(128) PRIMARY KEY, name VARCHAR(128) NOT NULL, last_name VARCHAR(128) NOT NULL, cellphone BIGINT DEFAULT 0);
+CREATE TABLE devices_elders (serial VARCHAR(128), elder_id VARCHAR(128), PRIMARY KEY (serial, elder_id), FOREIGN KEY (serial) REFERENCES panic_devices(serial), FOREIGN KEY (elder_id) REFERENCES elders(id));
+CREATE TABLE devices_history (serial VARCHAR(128) NOT NULL, elder_id VARCHAR(128) NOT NULL, attachmentDate TIMESTAMP NOT NULL, detachmentDate TIMESTAMP, FOREIGN KEY (serial) REFERENCES panic_devices(serial), FOREIGN KEY (elder_id) REFERENCES elders(id));
