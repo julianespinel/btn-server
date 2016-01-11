014b417675e079cae347fc58c9874c3a4869adca
Julian Espinel <julianespinel@gmail.com>
2016-01-10 19:33:10 -0500

    Improve db error handling.

--- panicdevice/persistence.go
+++ panicdevice/persistence.go
@@ -3,24 +3,30 @@ package panicdevice
 import (
 	"database/sql"
 	"time"
+
+	"github.com/Sirupsen/logrus"
 )
 
+var log = logrus.New()
+
 type PanicDAO struct {
+}
+
+func handleDBError(err error) {
+	if err != nil {
+		log.Error(err)
+	}
 }
 
 func createPanicDevice(database *sql.DB, device PanicDevice) PanicDevice {
 
 	stmt, err := database.Prepare("INSERT INTO panic_devices (serial, birth_date) VALUES (?, ?);")
-	if err != nil {
-		// Handle error properly.
-	}
+	handleDBError(err)
 	defer stmt.Close()
 
 	now := time.Now()
-	_, err = stmt.Exec(device.serial, now)
-	if err != nil {
-		// Handle error properly.
-	}
+	_, err = stmt.Exec(device.Serial, now)
+	handleDBError(err)
 
 	return device
 }
@@ -28,15 +34,11 @@ func createPanicDevice(database *sql.DB, device PanicDevice) PanicDevice {
 func attachElderToPanicDevice(database *sql.DB, deviceSerial string, elderId string) {
 
 	stmt, err := database.Prepare("INSERT INTO devices_elders (serial, elder_id) VALUES (? ,?);")
-	if err != nil {
-		//
-	}
+	handleDBError(err)
 	defer stmt.Close()
 
 	_, err = stmt.Exec(deviceSerial, elderId)
-	if err != nil {
-		//
-	}
+	handleDBError(err)
 
 	addElderToPanicDeviceHistory(database, deviceSerial, elderId)
 }
@@ -44,30 +46,22 @@ func attachElderToPanicDevice(database *sql.DB, deviceSerial string, elderId str
 func addElderToPanicDeviceHistory(database *sql.DB, deviceSerial string, elderId string) {
 
 	stmt, err := database.Prepare("INSERT INTO devices_history (serial, elder_id, attachment_date) VALUES (?, ?, ?);")
-	if err != nil {
-		//
-	}
+	handleDBError(err)
 	defer stmt.Close()
 
 	now := time.Now()
 	_, err = stmt.Exec(deviceSerial, elderId, now)
-	if err != nil {
-		//
-	}
+	handleDBError(err)
 }
 
 func detachElderFromPanicDevise(database *sql.DB, deviceSerial string, elderId string) {
 
 	stmt, err := database.Prepare("DELETE FROM devices_elders WHERE serial = ? AND elder_id = ?);")
-	if err != nil {
-		//
-	}
+	handleDBError(err)
 	defer stmt.Close()
 
 	_, err = stmt.Exec(deviceSerial, elderId)
-	if err != nil {
-		//
-	}
+	handleDBError(err)
 
 	removeElderToPanicDeviceHistory(database, deviceSerial, elderId)
 }
@@ -75,14 +69,10 @@ func detachElderFromPanicDevise(database *sql.DB, deviceSerial string, elderId s
 func removeElderToPanicDeviceHistory(database *sql.DB, deviceSerial string, elderId string) {
 
 	stmt, err := database.Prepare("UPDATE devices_history SET detachment_date = ? WHERE serial = ? AND elder_id = ?);")
-	if err != nil {
-		//
-	}
+	handleDBError(err)
 	defer stmt.Close()
 
 	now := time.Now()
 	_, err = stmt.Exec(now, deviceSerial, elderId)
-	if err != nil {
-		//
-	}
+	handleDBError(err)
 }
