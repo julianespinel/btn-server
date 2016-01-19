d4c90ba7b02c5606e385f365b4f0075db5e86df7
Julian Espinel <julianespinel@gmail.com>
2016-01-18 23:33:42 -0500

    Add relatives and elders_relatives tables.

--- db/migrations/1.0-create_tables.sql
+++ db/migrations/1.0-create_tables.sql
@@ -1,5 +1,12 @@
 -- +migrate Up
 CREATE TABLE panic_devices (serial VARCHAR(128) PRIMARY KEY, birth_date TIMESTAMP NOT NULL, death_date TIMESTAMP);
+
 CREATE TABLE elders (id VARCHAR(128) PRIMARY KEY, name VARCHAR(128) NOT NULL, last_name VARCHAR(128) NOT NULL, cellphone BIGINT DEFAULT 0, registration_date TIMESTAMP);
+
 CREATE TABLE devices_elders (serial VARCHAR(128), elder_id VARCHAR(128), PRIMARY KEY (serial, elder_id), FOREIGN KEY (serial) REFERENCES panic_devices(serial), FOREIGN KEY (elder_id) REFERENCES elders(id));
+
 CREATE TABLE devices_history (serial VARCHAR(128) NOT NULL, elder_id VARCHAR(128) NOT NULL, attachment_date TIMESTAMP NOT NULL, detachment_date TIMESTAMP, FOREIGN KEY (serial) REFERENCES panic_devices(serial), FOREIGN KEY (elder_id) REFERENCES elders(id));
+
+CREATE TABLE relatives (id VARCHAR(128) PRIMARY KEY, name VARCHAR(128) NOT NULL, last_name VARCHAR(128) NOT NULL, email VARCHAR(128), cellphone BIGINT DEFAULT 0, relationship VARCHAR(128));
+
+CREATE TABLE elders_relatives (elder_id VARCHAR(128), relative_id VARCHAR(128), PRIMARY KEY (elder_id, relative_id), FOREIGN KEY (elder_id) REFERENCES elders(id), FOREIGN KEY (relative_id) REFERENCES relatives(id));
